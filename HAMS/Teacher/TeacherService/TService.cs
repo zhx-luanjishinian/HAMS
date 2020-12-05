cbd197522f313cfe21b5e0d3c8b420aa3ab13a38
郑昊欣 <1291103474@qq.com>
2020-12-05 17:13:39 +0800

    添加了老师、管理员的登录

---
+++ HAMS/Teacher/TeacherService/TService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using HAMS.ToolClass;
+using HAMS.Teacher.TeacherDao;
+
+namespace HAMS.Teacher.TeacherService
+{
+    class TService
+    {
+        private TDao td = new TDao();
+
+        public BaseResult login(string account, string pw)
+        {
+            if (account == "" || pw == "")
+            {
+                return BaseResult.errorMsg("账号或者密码为空");
+            }
+            DataTable table = td.Login(account, pw);
+            if (table.Rows.Count == 0)
+            {
+                return BaseResult.errorMsg("账号或者密码输入错误，请检查后再进行输入");
+            }
+            else if (table.Rows[0][0].ToString() != pw)
+            {
+                return BaseResult.errorMsg("账号正确，密码错误");
+            }
+            else
+            {
+                return BaseResult.ok(table.Rows[0][1].ToString());
+            }
+
+        }
+    }
+}

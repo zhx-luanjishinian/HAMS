2d6e10925074a690f5aa75216daec306a558c12f
郑昊欣 <1291103474@qq.com>
2020-12-05 16:44:10 +0800

    修改了bug

--- HAMS/Student/StudentView/StudentMainForm.xaml.cs
+++ HAMS/Student/StudentView/StudentMainForm.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace HAMS.Student.StudentView
 {
@@ -20,12 +8,10 @@ namespace HAMS.Student.StudentView
     public partial class StudentMainForm : Window
     {
         public StudentMainForm(string session)
-
         {
             textBlockUserId.Text = session;
             InitializeComponent();
         }
-
         private void BtnHomewordAlert_Click(object sender, RoutedEventArgs e)
         {
 

be6e211546aa0f6de80c5433feaa3c54d9a60a57
郑昊欣 <1291103474@qq.com>
2020-12-10 17:53:16 +0800

    为了实现作业批改的初始改动

--- HAMS/Teacher/TeacherDao/TDao.cs
+++ HAMS/Teacher/TeacherDao/TDao.cs
@@ -1,4 +1,5 @@
-﻿using HAMS.Teacher.TeacherView;
+﻿using HAMS.Entity;
+using HAMS.Teacher.TeacherView;
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
@@ -39,11 +40,52 @@ namespace HAMS.Teacher.TeacherDao
         }
         public DataTable getNotice(string classSpaceId)  //从数据库查询目前已有的作业
         {
-            AnnounceNoticeDao temp = new AnnounceNoticeDao(); //为了使用查询classId的函数
-            DataTable table0 = temp.getClassId(classSpaceId);  //查询需要的classId
+            
+            DataTable table0 = getClassId(classSpaceId);  //查询需要的classId,使用查询classId的函数
             String sql = "select * from notice where classId = @id;";
             //传入要填写的参数
             MySqlParameter para = new MySqlParameter("@id", table0.Rows[0][0]);
+            DataTable table = DataUtil.DataOperation.DataQuery(sql, para);
+            return table;
+        }
+        public Boolean insertNotice(Notice notice)
+        {
+            String sql = "insert into notice (truDeadline,content,notURL,notTitle,classId) values (@truDdl,@cont,@ntUrl,@ntTitle,@cid);";
+            //传入要填写的参数
+            MySqlParameter para1 = new MySqlParameter("@truDdl", notice.TruDeadLine);
+            MySqlParameter para2 = new MySqlParameter("@cont", notice.Content);
+            MySqlParameter para3 = new MySqlParameter("@ntUrl", notice.NoteURL);
+            MySqlParameter para4 = new MySqlParameter("@ntTitle", notice.NoteTitle);
+            MySqlParameter para5 = new MySqlParameter("@cid", notice.ClassId);
+            return DataUtil.DataOperation.DataAdd(sql, para1, para2, para3, para4, para5);//如果插入成功，则返回true
+        }
+        public DataTable getClassId(string classSpecId)
+        {
+            //根据真实的课堂号获取课堂表里的自增主键课堂号classId
+            String sql = "select classId from class where classSpecId = @id;";
+            //传入要填写的参数
+            MySqlParameter para = new MySqlParameter("@id", classSpecId);
+            DataTable table = DataUtil.DataOperation.DataQuery(sql, para);
+            return table;
+
+        }
+        public DataTable getNoteTitle(int classId)
+        {
+            //从数据库表中
+            String sql = "select notTitle from notice where classId = @cid;";
+            //传入要填写的参数
+            MySqlParameter para = new MySqlParameter("@cid", classId);
+            DataTable table = DataUtil.DataOperation.DataQuery(sql, para);
+            return table;
+
+        }
+
+        public DataTable getTeacherId(string TeacherSpecId)
+        {
+            //根据教师工号获取课堂表里的自增主键教师号teacherId
+            String sql = "select teacherId from teacher where teacherSpecId = @tid;";
+            //传入要填写的参数
+            MySqlParameter para = new MySqlParameter("@tid", TeacherSpecId);
             DataTable table = DataUtil.DataOperation.DataQuery(sql, para);
             return table;
         }
--- HAMS/Teacher/TeacherView/TeacherHomeworkCheck.xaml.cs
+++ HAMS/Teacher/TeacherView/TeacherHomeworkCheck.xaml.cs
@@ -7,10 +7,12 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using HAMS.ToolClass;
 
 namespace HAMS.Teacher.TeacherView
 {
@@ -19,9 +21,56 @@ namespace HAMS.Teacher.TeacherView
     /// </summary>
     public partial class TeacherHomeworkCheck : Window
     {
-        public TeacherHomeworkCheck()
+        public TeacherHomeworkCheck(List<int> homIds,int index, string NotTitle,string StudentInfo)
         {
             InitializeComponent();
+            //通过上一个界面传递过来的值，进行此界面控件信息的赋值操作
+
+            //给作业公告标题赋值
+            lbNotTitle.Content = NotTitle;
+
+            //获取当前待批改作业的作业Id
+            int homId = homIds[index];//获得当前待批改作业的作业Id,此处还是一个假值
+            homId = 4;
+
+            //给学生信息赋值
+            lbStudentInfo.Content = StudentInfo;
+            //StudentInfo存储的是学号+1空格+姓名，按空格切分即可获得学号与姓名
+            string[] StudentInfos = StudentInfo.Split(' ');
+            string StuId = StudentInfos[0];//获取当前待批改作业的学号
+            string StuName = StudentInfos[1];//获取当前待批改作业的姓名
+
+            //对下载附件按钮进行初始化：能够实现鼠标放上去之后学生附件的名称，将该按钮和服务器上的某个路径建立关系（这里应该得到数据库中存储的文件路径）
+        }
+
+        private void btnUpload_Click(object sender, RoutedEventArgs e)
+        {
+
+            //1、从数据库中查出homURL,然后获取文件名并能够鼠标放上去时在旁边显示文件名
+            //从上一界面获取notId,根据notId+stuId访问到homId
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            //要保存的文件的文件名（"文件名和文件类型"）
+            string homURL = "we.txt";//数据库中的路径
+            sfd.FileName = "we.txt";//这个名字应该是从数据库中取到的文件名
+            //初始化提示保存文件的路径地址
+            sfd.InitialDirectory = "C:\\Users"; 
+
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+                string errorinfo;
+                //获取要保存文件名的完整路径
+                string localpath = sfd.FileName;
+                
+                
+                bool flag = FtpUpDown.Download(localpath, homURL, out errorinfo);
+                //localpath是本地路径,homURL是数据库中存放的文件路径（文件在服务器上的路径）
+                if (flag == true)
+                    System.Windows.MessageBox.Show("下载成功");
+                else
+                    System.Windows.MessageBox.Show("下载失败：" + errorinfo + "");
+            }
         }
     }
 }
